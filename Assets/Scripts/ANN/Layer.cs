6de32c9c71f1af4058dcd7b5b57e28e74787800d
Sven Laschinski <sven@laschinski.net>
2018-07-06 23:38:35 +0200

    Add neuron and layer script

---
+++ Assets/Scripts/ANN/Layer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer {
+
+    public List<Neuron> neurons = new List<Neuron>();
+
+    public Layer(int numberOfNeurons, int numberOfInputsPerNeuron)
+    {
+        for (int i = 0; i < numberOfNeurons; i++)
+        {
+            neurons.Add(new Neuron(numberOfInputsPerNeuron));
+        }
+    }
+}
---
+++ Assets/Scripts/ANN/Neuron.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neuron {
+    public double bias;
+    public double output;
+    public double errorGradient;
+    public List<double> inputs = new List<double>();
+    public List<double> weights = new List<double>();
+
+    // Initialize bias and weights with random values
+    public Neuron(int numberOfInputs)
+    {
+        for (int i = 0; i < numberOfInputs; i++)
+        {
+            weights.Add(UnityEngine.Random.Range(-1.0f, 1.0f));
+        }
+        bias = UnityEngine.Random.Range(-1.0f, 1.0f);
+    }
+}
